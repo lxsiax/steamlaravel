29b23154ae44d24585b8f85751002702b6c3daa4
Lucía <lucia.robles@iesdonana.org>
2025-11-10 21:27:24 +0100

    ruta nueva

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hola', function(Request $request){
+    $nombre = $request->query('nombre');
+    return 'Hola, $nombre';
 });
