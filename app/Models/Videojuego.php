0fb8d4b115438cdfc108e8bfb479df61061307fc
Lucía <lucia.robles@iesdonana.org>
2025-11-19 18:16:03 +0100

    desarrolladoras y juegos

---
+++ app/Models/Videojuego.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Videojuego extends Model
+{
+    protected $fillable = [
+        'nombre',
+        'precio',
+        'lanzamiento',
+        'desarrolladora_id',
+    ];
+    public function desarrolladora(): BelongsTo
+    {
+        return $this->belongsTo(Desarrolladora::class);
+    }
+}
